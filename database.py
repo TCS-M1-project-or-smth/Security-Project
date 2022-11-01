53533e4de85b373ece89d52dab67429bc9791acd
Melle Bosboom <mellebosboom@hotmail.com>
2022-11-01 15:29:11 +0100

    Close connection on destruction of Database object also added some code to the serial reading

--- database.py
+++ database.py
@@ -3,8 +3,9 @@ import sqlite3
 
 class Database:
 	def __init__(self):
-		self.connection = sqlite3.connect("system.db")
-		self.connection.execute("""CREATE TABLE IF NOT EXISTS cardholders(
+		self.__connection = sqlite3.connect("system.db")
+		self.cursor = self.__connection.cursor()
+		self.cursor.execute("""CREATE TABLE IF NOT EXISTS cardholders(
 			uid INT,
 			bal SMALLINT,
 			full_name varchar,
@@ -12,3 +13,9 @@ class Database:
 			clearance int,
 			counterval tinyint
 		)""")
+
+	def __del__(self):
+		self.__connection.close()
+
+	def get_bal_from_uid(self, uid):
+		self.cursor.execute("SELECT bal FROM cardholders WHERE uid=?", uid)
