da42776ec6d147d04c8af11ec0513d22e794869d
Melle Bosboom <mellebosboom@hotmail.com>
2022-11-03 19:05:31 +0100

    Sync for Desktop

--- database.py
+++ database.py
@@ -1,28 +1,57 @@
 import sqlite3
 import os
 import rsa  # pip install rsa
+from datetime import datetime
 
 
 class Database:
 	def __init__(self):
 		self.__connection = sqlite3.connect("system.db")
+		self.__connection.execute("PRAGMA foreign_keys = 1")
 		self.queue: list[tuple[callable, list]] = []  # (method, *args)
 		self.running = True
 		self.cursor = self.__connection.cursor()
-		self.cursor.execute("""CREATE TABLE IF NOT EXISTS campuscard(
-			uid BLOB PRIMARY KEY,
-			bal SMALLINT,
-			full_name TEXT,
-			person_id TEXT,
-			clearance int,
-			counterval tinyint
-		)""")
 
-		self.publkey, self.privkey = rsa.newkeys(512)
-		with open("public.pem", "wb") as f:
-			f.write(self.publkey.save_pkcs1(format='PEM'))
-		with open("private.pem", "wb") as f:
-			f.write(self.privkey.save_pkcs1(format='PEM'))
+		# Create tables
+		self.cursor.execute("""CREATE TABLE IF NOT EXISTS clearances(
+					clearance_id INT PRIMARY KEY,
+					description TEXT, -- usually department like Physics or Security or TCS M1 Coordinator
+					category INT -- 4 bytes of flag, make sure to remove sign the int when checking flags
+				)""")
+		self.cursor.execute("""CREATE TABLE IF NOT EXISTS campuscards(
+					uid TEXT PRIMARY KEY,
+					bal FLOAT,
+					full_name TEXT,
+					date_of_birth DATE,
+					person_id TEXT,
+					clearance_id INTEGER,
+					blocked INTEGER DEFAULT 0, -- 0 is false, 1 is true
+					counterval INTEGER,
+					FOREIGN KEY (clearance_id) REFERENCES clearances(clearance_id)
+				)""")
+		self.cursor.execute("""CREATE TABLE IF NOT EXISTS readers(
+			reader_id INT PRIMARY KEY,
+			clearance_id INTEGER,
+			action_type INTEGER, -- 0 is payment, 1 is door authorization, 2 is top up balance
+			FOREIGN KEY (clearance_id) REFERENCES clearances(clearance_id)
+		)""")
+		self.cursor.execute("""CREATE TABLE IF NOT EXISTS transactions(
+			person_id TEXT,
+			time_stamp DATE,
+			reader_id INTEGER,
+			amount FLOAT, -- positive amount means reader to person, negative is person to reader
+			status INTEGER DEFAULT 0, -- 0 is being processed, 1 is approved, 2 is declined
+			PRIMARY KEY (person_id, reader_id, time_stamp),
+			FOREIGN KEY (person_id) REFERENCES campuscards(person_id),
+			FOREIGN KEY (reader_id) REFERENCES readers(reader_id)
+		)""")
+		self.__connection.commit()
+
+		# self.publkey, self.privkey = rsa.newkeys(512)
+		# with open("public.pem", "wb") as f:
+		# 	f.write(self.publkey.save_pkcs1(format='PEM'))
+		# with open("private.pem", "wb") as f:
+		# 	f.write(self.privkey.save_pkcs1(format='PEM'))
 
 		with open("public.pem", "rb") as f:
 			data = f.read()
@@ -44,35 +73,109 @@ class Database:
 	def tick(self):
 		while self.running or len(self.queue) > 0:
 			if len(self.queue) > 0:
-				self.queue[0][0](*self.queue[0][1])
+				try:
+					self.queue[0][0](*self.queue[0][1])
+				except Exception as e:
+					print(e)
 				self.queue.pop(0)
 
+	def cuid(self, uid):
+		return rsa.sign(str(uid).encode("utf-8"), self.privkey, 'SHA-256')
+
+	def encrypt(self, msg: str):
+		return rsa.encrypt(msg.encode('utf-8'), self.publkey)
+
+	def check_blocked(self, uid):
+		self.cursor.execute("SELECT blocked FROM campuscards WHERE uid=?", [self.cuid(uid)])
+		fetch = self.cursor.fetchone()
+		return fetch is not None and fetch[0]
+
+	def check_counter_val(self, uid, counter):
+		self.cursor.execute("SELECT counter FROM campuscards WHERE uid=?", [self.cuid(uid)])
+		fetch = self.cursor.fetchone()
+		return fetch is not None and fetch[0] == counter
+
+	def mark_blocked(self, uid):
+		self.cursor.execute("UPDATE TABLE campuscards SET blocked = 1 WHERE uid=?", [self.cuid(uid)])
+
+	def get_action_type_from_reader(self, rid):
+		self.cursor.execute("SELECT action_type FROM readers WHERE reader_id=?", [rid])
+
 	def get_bal_from_uid(self, uid):
-		cuid = rsa.sign(str(uid).encode("utf-8"), self.privkey, 'SHA-256')
-		self.cursor.execute("SELECT bal, full_name FROM campuscard WHERE uid=?", [cuid])
+		self.cursor.execute("SELECT bal, full_name FROM campuscards WHERE uid=?", [self.cuid(uid)])
 		fetch = self.cursor.fetchone()
 		print(fetch[0], rsa.decrypt(fetch[1], self.privkey))
 
-	def insert(self, *args):  # args: [uid, bal, full_name, person_id, clearance, counterval]
-		if len(args) != 6:
+	def insert_campuscard(self, *args):  # args: [uid, bal, full_name, dob, person_id, clearance_id, blocked, counterval]
+		if len(args) != 8:
+			print(f"INSERT expected 8 arguments, got {len(args)}")
 			return
-		cuid = rsa.sign(args[0].encode("utf-8"), self.privkey, 'SHA-256')
-		cname = rsa.encrypt(args[3].encode('utf-8'), self.publkey)
-		cpid = rsa.encrypt(str(args[3]).encode("utf-8"), self.publkey)
+
 		try:
-			self.cursor.execute("INSERT INTO campuscard VALUES (?, ?, ?, ?, ?, ?)",
-							[cuid, int(args[1]), cname, cpid, int(args[4]), int(args[5])])
+			self.cursor.execute("INSERT INTO campuscards VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+								[self.cuid(args[0]), int(args[1]), self.encrypt(args[2]),
+								 datetime.strptime(args[3], "%d-%m-%Y"), self.encrypt(args[4]), int(args[5]),
+								 args[6], int(args[7])])
 			print(self.get_bal_from_uid(args[0]))
+			self.__connection.commit()
+		except ValueError:
+			print("Failed to convert to int")
+	
+	def insert_reader(self, *args):  # args: [rid, clearance_id, action_type]
+		if len(args) != 8:
+			print(f"INSERT expected 8 arguments, got {len(args)}")
+			return
+
+		try:
+			self.cursor.execute("INSERT INTO readers VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+							[self.cuid(args[0]), int(args[1]), self.encrypt(args[2]), datetime.strptime(args[3], "%d-%m-%Y"), self.encrypt(args[4]), int(args[5]),
+							 args[6], int(args[7])])
+			print(self.get_bal_from_uid(args[0]))
+			self.__connection.commit()
 		except ValueError:
 			print("Failed to convert to int")
 
 	def select(self, *args):  # TODO: temporary maybe
 		cuid = rsa.sign(args[0].encode("utf-8"), self.privkey, 'SHA-256')
-		self.cursor.execute("SELECT * FROM campuscard WHERE uid=?", [cuid])
+		self.cursor.execute("SELECT * FROM campuscards WHERE uid=?", [cuid])
 		fetch = self.cursor.fetchone()
 		print(args[0], fetch[1], rsa.decrypt(fetch[2], self.privkey),
 				rsa.decrypt(fetch[3], self.privkey), fetch[4], fetch[5])
 
-	def update_bal_from_uid(self, uid, bal):
-		cuid = rsa.sign(str(uid).encode("utf-8"), self.privkey, 'SHA-256')
-		self.cursor.execute("UPDATE campuscard SET bal = bal + ? WHERE uid = ? ", [bal, cuid])
+	def has_sufficient_clearance(self, uid, rid):
+		self.cursor.execute("SELECT clearance_id FROM campuscards WHERE uid=?", [self.cuid(uid)])
+		fetch1 = self.cursor.fetchone()
+		self.cursor.execute("SELECT clearance_id FROM readers WHERE reader_id=?", [rid])
+		fetch2 = self.cursor.fetchone()
+		return fetch1 is not None and fetch2 is not None and fetch1[0] == fetch2[0]
+
+	def complete_transaction(self, pid, timestamp, rid):
+		self.cursor.execute("SELECT amount FROM transactions WHERE pid=? and time_stamp=? and reader_id=?", [pid, timestamp, rid])
+		fetch = self.cursor.fetchone()
+		if fetch is None:
+			self.cursor.execute("UPDATE transactions SET status=2 WHERE pid=? and time_stamp=? and reader_id=?", [pid, timestamp, rid])
+			self.__connection.commit()
+			return
+		self.cursor.execute("UPDATE campuscards SET bal=bal+?", fetch)
+		self.cursor.execute("UPDATE transactions SET status=1 WHERE pid=? and time_stamp=? and reader_id=?", [pid, timestamp, rid])
+		self.__connection.commit()
+
+	def create_transaction(self, uid, rid, amount):
+		self.cursor.execute("SELECT person_id FROM campuscards WHERE uid=?", [self.cuid(uid)])
+		fetch = self.cursor.fetchone()
+		if fetch is None:
+			return None
+		pid = fetch[0]  # pid is encrypted
+		timestamp = datetime.now()
+		try:
+			self.cursor.execute("INSERT INTO transactions VALUES (?, ?, ?, ?)", [pid, timestamp, rid, amount])
+			self.__connection.commit()
+		except Exception as e:
+			print(f"Failed to create transaction:")
+			print(e)
+			return None
+		return pid, timestamp
+
+	def queue_transaction_payment(self, pid, timestamp, rid):  # OOP moment
+		self.queue.append((self.complete_transaction, [pid, timestamp, rid]))
+
