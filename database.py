7d21a89248cf7bf4a56b61caa143d76d11105470
Melle Bosboom <mellebosboom@hotmail.com>
2022-11-03 11:11:17 +0100

    Create start of admin

--- database.py
+++ database.py
@@ -6,8 +6,10 @@ import rsa  # pip install rsa
 class Database:
 	def __init__(self):
 		self.__connection = sqlite3.connect("system.db")
+		self.queue: list[tuple[callable, list]] = []  # (method, *args)
+		self.running = True
 		self.cursor = self.__connection.cursor()
-		self.cursor.execute("""CREATE TABLE IF NOT EXISTS cardholders(
+		self.cursor.execute("""CREATE TABLE IF NOT EXISTS campuscard(
 			uid BLOB PRIMARY KEY,
 			bal SMALLINT,
 			full_name TEXT,
@@ -39,14 +41,38 @@ class Database:
 			f.write(self.privkey.save_pkcs1(format='PEM'))
 		self.__connection.close()
 
+	def tick(self):
+		while self.running or len(self.queue) > 0:
+			if len(self.queue) > 0:
+				self.queue[0][0](*self.queue[0][1])
+				self.queue.pop(0)
+
 	def get_bal_from_uid(self, uid):
 		cuid = rsa.sign(str(uid).encode("utf-8"), self.privkey, 'SHA-256')
-		self.cursor.execute("SELECT bal, full_name FROM cardholders WHERE uid=?", [cuid])
+		self.cursor.execute("SELECT bal, full_name FROM campuscard WHERE uid=?", [cuid])
 		fetch = self.cursor.fetchone()
 		print(fetch[0], rsa.decrypt(fetch[1], self.privkey))
 
-	def insert(self):
-		cuid = rsa.sign(str(1234).encode("utf-8"), self.privkey, 'SHA-256')
-		cname = rsa.encrypt("Melle Bosboom".encode('utf-8'), self.publkey)
-		self.cursor.execute("""INSERT INTO cardholders VALUES (?, 2000, ?, 42069, 69, 420)
-		""", [cuid, cname])
+	def insert(self, *args):  # args: [uid, bal, full_name, person_id, clearance, counterval]
+		if len(args) != 6:
+			return
+		cuid = rsa.sign(args[0].encode("utf-8"), self.privkey, 'SHA-256')
+		cname = rsa.encrypt(args[3].encode('utf-8'), self.publkey)
+		cpid = rsa.encrypt(str(args[3]).encode("utf-8"), self.publkey)
+		try:
+			self.cursor.execute("INSERT INTO campuscard VALUES (?, ?, ?, ?, ?, ?)",
+							[cuid, int(args[1]), cname, cpid, int(args[4]), int(args[5])])
+			print(self.get_bal_from_uid(args[0]))
+		except ValueError:
+			print("Failed to convert to int")
+
+	def select(self, *args):  # TODO: temporary maybe
+		cuid = rsa.sign(args[0].encode("utf-8"), self.privkey, 'SHA-256')
+		self.cursor.execute("SELECT * FROM campuscard WHERE uid=?", [cuid])
+		fetch = self.cursor.fetchone()
+		print(args[0], fetch[1], rsa.decrypt(fetch[2], self.privkey),
+				rsa.decrypt(fetch[3], self.privkey), fetch[4], fetch[5])
+
+	def update_bal_from_uid(self, uid, bal):
+		cuid = rsa.sign(str(uid).encode("utf-8"), self.privkey, 'SHA-256')
+		self.cursor.execute("UPDATE campuscard SET bal = bal + ? WHERE uid = ? ", [bal, cuid])
--- main.py
+++ main.py
@@ -1,27 +1,88 @@
 import sys
 from random import randint  # for simulating different actions
 import serial  # pip install pyserial
+import rsa
+import threading
 from database import Database
 
 
 class Main:
-    def __init__(self):
-        self.database = Database()
-        self.database.insert()
-        self.database.get_bal_from_uid(1234)
-        #self.run_serial()
-        self.shutdown()
+	def __init__(self):
+		self.database = Database()
+		self.publkey, self.privkey = rsa.newkeys(512)
+		# For testing
+		#self.database.insert()
+		#self.database.get_bal_from_uid(1234)
+		self.apublkey = None
+		self.serialthread = threading.Thread(target=self.run_serial, daemon=True)
+		# self.dbthread = threading.Thread(target=self.database.tick, daemon=True)
+		# self.dbthread.start()
+		# self.serialthread.start()
+		self.adthread = threading.Thread(target=self.admin, daemon=True)
+		self.adthread.start()
+		self.database.tick()
+		self.shutdown()
 
-    def run_serial(self):
-        with serial.Serial(sys.argv[0] if ["COM3", "COM4", "COM5"] in sys.argv else "COM5", 9600) as ser:
-            uid = ser.read(4)
-            print(f"{uid=}")
-            self.database.get_bal_from_uid(uid)
+	def admin(self):
+		while True:
+			inp = input("$> ")
+			if inp.lower() == "exit" or inp == "":
+				self.database.running = False
+				return
 
-    def shutdown(self):
-        del self.database
+			split = inp.split()
+			match split[0].upper():
+				case "INSERT":
+					self.database.queue.append((self.database.insert, split[1:]))
+				case "SELECT":
+					self.database.queue.append((self.database.select, split[1:]))
+
+	def run_serial(self):
+		with serial.Serial(sys.argv[0] if ["COM3", "COM4", "COM5"] in sys.argv else "COM5", 9600) as ser:
+			def init():
+
+				# initialize encrypted connection with Arduino
+				ser.write(self.database.publkey.e)
+				ser.write(self.database.publkey.n)
+
+				# TODO: See how many bytes actually need to be read
+				e = ser.read(512)
+				n = ser.read(512)
+				self.apublkey = rsa.PublicKey(n, e)
+
+			def send_data(data):
+				ser.write(rsa.encrypt(data, self.apublkey))
+
+			def read_data(byte):
+				return ser.read(byte)
+
+			init()
+
+			# get uid
+			uid = ser.read(4)
+			print(f"{uid=}")
+			self.database.get_bal_from_uid(uid)
+
+			auth_code = 0  # 0 == success, 1 == failure, 2 == insufficient balance
+			match randint(0, 11):
+				case 0:  # buy coffee
+					if self.database.get_bal_from_uid(uid) > 0.20:
+						auth_code = 0
+						self.database.update_bal_from_uid(uid)
+					else:
+						auth_code = 2
+
+				case 1:
+					pass
+				case 2:
+					pass
+			auth_code = str(auth_code).encode('utf-8')
+			send_data(auth_code)
+
+	def shutdown(self):
+		del self.database
 
 
 if __name__ == '__main__':
-    main = Main()
+	main = Main()
 
